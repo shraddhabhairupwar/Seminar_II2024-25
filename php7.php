eee65745493149aac1b8657f8dbf7ce265d9c12d
shraddhabhairupwar <s23_bhairupwar_shraddha@mgmcen.ac.in>
2025-07-15 20:23:08 +0530

    Add files via upload
    PHP program to display a digital clock

---
+++ php7.php
@@ -0,0 +1,18 @@
+<head>
+<meta http-equiv="refresh" content="1"/>
+<style>
+p {
+color:black;
+font-size:100px;
+position:absolute;
+
+top: 40%;
+left: 50%;
+transform: translate(-50%, -50%);
+}
+body {
+background-color:pink;
+}
+</style>
+<p> <?php echo date(" h: i : s A");?> </p>
+</head>
